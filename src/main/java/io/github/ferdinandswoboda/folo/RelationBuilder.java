97bee9e94cfe012408e0341402745422c4faea39
Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>
2022-04-16 22:08:42 +0200

    Replace FindBugs dependency

--- src/main/java/io/github/ferdinandswoboda/folo/RelationBuilder.java
+++ src/main/java/io/github/ferdinandswoboda/folo/RelationBuilder.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 import org.jooq.Field;
 import org.jooq.ForeignKey;
 import org.jooq.Record;
--- src/test/java/io/github/ferdinandswoboda/folo/TestUtil.java
+++ src/test/java/io/github/ferdinandswoboda/folo/TestUtil.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 import org.jooq.DSLContext;
 import org.jooq.Field;
 import org.jooq.Record;
