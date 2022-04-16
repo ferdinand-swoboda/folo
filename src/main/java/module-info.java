7370ac4779a6982a9cf90bc509daa56b234e1312
Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>
2022-04-16 21:58:36 +0200

    Add module descriptor

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module folo {
+  requires java.compiler;
+  requires transitive org.jooq;
+  requires static jsr305;
+  requires static com.google.errorprone.annotations;
+  requires static org.immutables.value.annotations;
+
+  exports io.github.ferdinandswoboda.folo;
+
+  uses org.jooq.Field;
+  uses org.jooq.Record;
+  uses org.jooq.Table;
+  uses org.jooq.TableField;
+  uses org.jooq.ForeignKey;
+  uses org.jooq.UniqueKey;
+}
